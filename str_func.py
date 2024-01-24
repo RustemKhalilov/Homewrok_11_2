b85841306a46c24f23af8f4600c71b901f69134a
Rustem Khalilov <rvkhalilov@gmail.com>
2024-01-25 00:53:45 +0500

    add new function Simvol_UP_first

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def Simvol_UP(word):
-    return word.upper
+    """Функция возвращает все буквы слова заглавными"""
+    return word.upper()
+
+
+def Simvol_UP_first(words):
+    """Функция возвращает первые буквы слов заглавными"""
+    str1 = words.split(" ")
+    str_2 = ""
+    for item in str1:
+        str_2 = str_2 + " " + item[0].upper() + item[1:]
+    return str_2
+
+
+
